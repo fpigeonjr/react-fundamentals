ec1649fc9bebbb9e6ac0b60c76e54f66bb604492
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-10-15 17:35:51 -0500

    solution to challenge 1

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -12,26 +12,41 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div className="box box--small" style={{backgroundColor: "lightblue", fontStyle: 'italic'}}>small lightblue box</div>
-const mediumBox = <div className="box box--medium" style={{backgroundColor: "pink", fontStyle: 'italic'}}>medium pink box</div>
-const largeBox = <div className="box box--large" style={{backgroundColor: "orange", fontStyle: 'italic'}}>large orange box</div>
-
-const Box  = (props) =>  {
+const Box = ({className = '', style = '', ...otherProps}) => {
   return (
-    <div className={`box ${props.className}`} style={props.style}>
-      {props.children}
-    </div>
+    <div
+      className={`box ${className}`}
+      style={{fontStyle: 'italic', ...style}}
+      {...otherProps}
+    />
   )
 }
 
+const smallBox = (
+  <Box className="box box--small" style={{backgroundColor: 'lightblue'}}>
+    small lightblue box
+  </Box>
+)
+const mediumBox = (
+  <Box className="box box--medium" style={{backgroundColor: 'pink'}}>
+    medium pink box
+  </Box>
+)
+const largeBox = (
+  <Box className="box box--large" style={{backgroundColor: 'orange'}}>
+    large orange box
+  </Box>
+)
+
 function App() {
   return (
-  <div>
-    <Box className="box--small" style={{backgroundColor: "lightblue", fontStyle: 'italic'}}> small lightblue box</Box>
-    {smallBox}
-    {mediumBox}
-    {largeBox}
-  </div>
+    <div>
+      {smallBox}
+      {mediumBox}
+      {largeBox}
+
+      <Box>sizeless box</Box>
+    </div>
   )
 }
 
